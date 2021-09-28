3e5457e4b88f19e4100f10225123e4dbe768a91a
rvanooyen <robert.vanooyen@gmail.com>
2021-09-28 18:52:43 -0500

    update 2 capture input

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
-// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'Please enter the readme title. (Requiered)',
+            validate: titleInput => {
+                if (titleInput) {
+                    questions.push(titleInput);
+                    return true;
+                } else {
+                    console.log('Please enter your readme title!');
+                    return false;
+                }
+            }
+        }
+    ])    
+};
+
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeToFile = (fileName, data) => {
+
+};
 
 // TODO: Create a function to initialize app
-function init() {
-    console.log(inquirer);
-}
+const init = () => {
+    // console.log(questions);
+};
+
+// Function call to prompt user
+promptUser();
 
 // Function call to initialize app
 init();
